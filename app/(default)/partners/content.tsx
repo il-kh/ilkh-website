f7ac4fe0c92593bb815731947f49aab2ae8195b5
Oellers, Daniel <do.dev@outlook.com>
2025-08-27 21:49:33 +0700

    fix: link corp partner

--- app/(default)/partners/content.tsx
+++ app/(default)/partners/content.tsx
@@ -11,12 +11,12 @@ export default async function PartnerContent() {
           {/* Content */}
           <div className="md:min-w-[30rem]" data-aos="fade-left">
             <div className="text-std border-l-2 border-slate-800 pl-4 mb-8">
-              <a href="https://groundsearch.biz/">Groundsearch</a> is a company with 30 years of experience that helps people find things underground. They use special technologies to help engineers and people looking for resources like minerals and water. They also help people find underground hazards like voids and weak spots. They work with existing projects to map resources and find geological hazards. If you need help with something underground, contact Groundsearch.
+              <a href="https://groundsearch.biz/" target="_blank">Groundsearch</a> is a company with 30 years of experience that helps people find things underground. They use special technologies to help engineers and people looking for resources like minerals and water. They also help people find underground hazards like voids and weak spots. They work with existing projects to map resources and find geological hazards. If you need help with something underground, contact Groundsearch.
             </div>
           </div>
           {/* Image */}
           <div className="flex justify-center items-center" data-aos="fade-right">
-            <Link href="https://groundsearch.biz/" className="relative">
+            <Link href="https://groundsearch.biz/" target="_blank" className="relative">
               <div className="absolute inset-0 pointer-events-none border-2 border-slate-200 -translate-x-4 -translate-y-4 -z-10" aria-hidden="true"></div>
               <Image className="mx-auto md:max-w-none" src={PartnerImage01} width={540} height={405} alt="GroundSearch Asia"/>
             </Link>
